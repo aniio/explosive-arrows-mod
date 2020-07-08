34ba28b450ec8ab507712662b4b47ef335d1736c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-08 13:51:07 -0700

    version 1.0.2, removed notes in ModItems.java

--- src/main/java/com/ani/explosivearrows/ModItems.java
+++ src/main/java/com/ani/explosivearrows/ModItems.java
@@ -9,11 +9,9 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class ModItems {
-
-    //The ITEMS deferred register in which you can register items.
+    
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ExplosiveArrow.MOD_ID);
 
-    //Register the tutorial dust with "tutorial_dust" as registry name and default properties
     public static final RegistryObject<Item> EXPLOSIVE_ARROW = ITEMS.register("explosive_arrow", () -> new Item(new Item.Properties().maxStackSize(64).group(ItemGroup.COMBAT)));
 
 }
