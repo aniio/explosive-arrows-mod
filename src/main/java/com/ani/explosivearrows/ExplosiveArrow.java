7536cad941bf96249105391b8641ecfc4ab3f332
BuildTools <unconfigured@null.spigotmc.org>
2020-07-08 11:04:50 -0700

    mod now appears in game

---
+++ src/main/java/com/ani/explosivearrows/ExplosiveArrow.java
@@ -0,0 +1,14 @@
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * The main class of the mod, this is the class that looks like a mod to forge.
+ */
+@Mod(ExplosiveArrow.MOD_ID)
+public class ExplosiveArrow {
+
+    /**
+     * The modid of this mod, this has to match the modid in the mods.toml and has to be in the format defined in {@link net.minecraftforge.fml.loading.moddiscovery.ModInfo}
+     */
+    public static final String MOD_ID = "eamod";
+
+}
